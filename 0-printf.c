f05d7b0210ee6f200587edd863ddc15d11a4618e
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-25 18:04:20 +0000

    Adding new file

--- 0-printf.c
+++ 0-printf.c
@@ -3,6 +3,18 @@
 #include <stdarg.h>
 
 /**
+ * print_s - Prints string
+ * @arg: The argument
+ */
+void print_s(char *arg)
+{
+	while (*arg != '\0')
+	{
+		_putchar(*arg);
+		arg++;
+	}
+}
+/**
  * _printf - Prints the output according to a certain format
  * @format: The format to be followed
  * @...: The arguments to be passed
@@ -13,40 +25,34 @@ int _printf(const char *format, ...)
 {
 	int i = 0, len = 0;
 	va_list args;
-	char *s;
 
 	va_start(args, format);
 	if (format != NULL)
 	{
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
+		while (format[i] != '\0')
 		{
-			len++;
-			switch (format[i + 1])
+			if (format[i] == '%')
 			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					break;
-				case 's':
-					s = va_arg(args, char *);
-					while (*s != '\0')
-					{
-						_putchar(*s);
-						s++;
-					}
-					break;
-				default:
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					break;
+				len++;
+				switch (format[i + 1])
+				{
+					case 'c':
+						_putchar(va_arg(args, int));
+						break;
+					case 's':
+						print_s(va_arg(args, char *));
+						break;
+					default:
+						_putchar(format[i]);
+						_putchar(format[i + 1]);
+						break;
+				}
+				i++;
 			}
+			else
+				_putchar(format[i]);
 			i++;
 		}
-		else
-			_putchar(format[i]);
-		i++;
-	}
 	}
 	va_end(args);
 	return (len);
